824a0317e756f97e65a9794d2754305ca1e4c569
Luis Leandro <luchrist777@gmail.com>
2020-05-17 04:22:16 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -1,29 +1,9 @@
-require_relative "../lib/birthday.rb"
+def age_appropriate_birthday(birthday_kids)
 
-describe "#happy_birthday" do
-  it "prints out a birthday message to each kid in the birthday_kids hash" do
-    birthday_kids = {
-      "Timmy" => 9,
-      "Sarah" => 6,
-      "Amanda" => 27
-    }
-
-    expect{happy_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout
-
+  birthday_kids.each do |kids_name, age|
+    if age <=12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
   end
-end
-
-## BONUS ##
-## Change "xit" to "it" in the test block to enable
-describe "#age_appropriate_birthday" do
-  xit "only prints the birthday greeting if the birthday kid 12 or younger" do
-    birthday_kids = {
-      "Timmy" => 9,
-      "Sarah" => 6,
-      "Amanda" => 27
-    }
-
-    expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
-
-  end
-end
